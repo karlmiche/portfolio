e435f627a90ff34fc7306958917da6d7632ad1f8
karlmiche <karlymism@gmail.com>
2020-08-07 16:14:30 -0700

    some minor styling

---
+++ components/navbar.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import {useEffect} from 'react'
+
+export default function Navbar () {
+
+    // how to do DOM manipulation in react
+    // useEffect(() => {
+    //     document.getElementById("home").style.color = "hotpink"
+    // }, [])
+    
+    return (
+        <div>
+            <Head>
+               <title>kms 🐈</title>
+            </Head>
+            <navbar>
+                <ul>
+                    <li><Link href="/"><a id="home">Home</a></Link></li>
+                    <li><Link href="/about"><a>About</a></Link></li>
+                </ul>
+            </navbar>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          karly smith
+        🐈  karly smith 🐈
         </h1>
 
         <p className={styles.description}>
